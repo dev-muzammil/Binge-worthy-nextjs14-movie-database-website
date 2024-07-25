ef636b71921437c7a6369bbe62c93d2e6bb5d0c9
Muzammil Shaikh <dev.muzammil786@gmail.com>
2024-07-25 20:02:21 +0500

    Fixed Production Bugs

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,16 +1,27 @@
-"use client"
-import Link from 'next/link'
-import React from 'react'
-import { useSearchParams } from 'next/navigation'
+"use client";
+import Link from "next/link";
+import React, { Suspense } from "react";
+import { useSearchParams } from "next/navigation";
 
-const NavbarItem = ({title, param}) => {
-    const searchParams = useSearchParams()
-    const genre = searchParams.get('genre')
+const NavbarItem = ({ title, param }) => {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get("genre");
   return (
-    <div className='hover:text-emerald-100'>
-        <Link className={`${genre === param ? 'underline underline-offset-8 decoration-4 decoration-emerald-900 rounded-lg' : ""}`} href={`/?genre=${param}`}>{title}</Link>
-    </div>
-  )
-}
+    <Suspense fallback={<div>Loading search parameters...</div>}>
+      <div className="hover:text-emerald-100">
+        <Link
+          className={`${
+            genre === param
+              ? "underline underline-offset-8 decoration-4 decoration-emerald-900 rounded-lg"
+              : ""
+          }`}
+          href={`/?genre=${param}`}
+        >
+          {title}
+        </Link>
+      </div>
+    </Suspense>
+  );
+};
 
-export default NavbarItem
+export default NavbarItem;
